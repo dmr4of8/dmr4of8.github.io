59cf4ec817a6f44f3afb2c65659b2dea264218cc
robertsdana <dmr4of8@gmail.com>
2020-06-27 21:37:23 -0600

    week 10 preston weather api

--- js/preston-townpage.js
+++ js/preston-townpage.js
@@ -17,3 +17,45 @@ const alertBanner = document.querySelector(".important-alert");
 if (dayNum !== 5) {
     alertBanner.classList.add("hidden");
 }
+
+const appID = '75ec8f9280bbd969ed410bd87a0de696';
+const prestonID = '5604473';
+const apiURL = `http://api.openweathermap.org/data/2.5/weather?id=${prestonID}&units=imperial&appid=${appID}`;
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    document.getElementById("currently").innerText = jsObject.weather[0].main;
+    document.getElementById("high-temp").innerText = jsObject.main.temp_max;
+    document.getElementById("humidity").innerText = jsObject.main.humidity;
+    document.getElementById("wind-speed").innerText = jsObject.wind.speed;
+});
+
+var weekdays = new Array(7)
+weekdays[0] = "Sun";
+weekdays[1] = "Mon";
+weekdays[2] = "Tue";
+weekdays[3] = "Wed";
+weekdays[4] = "Thu";
+weekdays[5] = "Fri";
+weekdays[6] = "Sat";
+
+const forecastURL = `http://api.openweathermap.org/data/2.5/forecast?id=${prestonID}&units=imperial&appid=${appID}`;
+fetch(forecastURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    for (i=0; i < jsObject.list.length; i++) {
+        if (jsObject.list[i].dt_txt.includes("18:00:00")) {
+            const forecastDate = new Date(jsObject.list[i].dt_txt);
+            const today = new Date();
+            const forecastNumber = Math.ceil((forecastDate - today) / (1000 * 3600 * 24));
+            document.getElementById(`fore-${forecastNumber}`).innerText = Math.floor(jsObject.list[i].main.temp);
+            document.getElementById(`day-${forecastNumber}`).innerText = weekdays[forecastDate.getDay()];
+            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';
+            const desc = jsObject.list[i].weather[0].description;
+            document.getElementById(`icon-${forecastNumber}`).setAttribute('src', imagesrc);  
+            document.getElementById(`icon-${forecastNumber}`).setAttribute('alt', desc);
+        }
+    }
+});
+
+
--- js/windchill.js
+++ js/windchill.js
@@ -1,14 +1,18 @@
 const windChill = document.getElementById("wind-chill");
 const oldInnerText = windChill.innerText;
 
-const high = parseInt(document.getElementById("high-temp").innerText);
-const wind = parseInt(document.getElementById("wind-speed").innerText);
-var s = Math.pow(wind,0.16);
-var wc = 35.74 + (0.6215*high) + (0.4275*high*s) - (35.75*s);
-
-if (high > 50 && wind < 3) {
-    windChill.innerText = `${oldInnerText} N/A`;
-}
-else {
-    windChill.innerText = `${oldInnerText} ${Math.round(wc)}°F`;
-}
+fetch(apiURL)
+  .then((response) => response.json())
+  .then((jsObject) => {
+    const high = jsObject.main.temp_max;
+    const wind = jsObject.wind.speed;
+    var s = Math.pow(wind,0.16);
+    var wc = 35.74 + (0.6215*high) + (0.4275*high*s) - (35.75*s);
+    
+    if (high > 50 && wind < 3) {
+        windChill.innerText = `${oldInnerText} N/A`;
+    }
+    else {
+        windChill.innerText = `${oldInnerText} ${Math.round(wc)}°F`;
+    }
+});
