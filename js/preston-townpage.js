e744982ad241bb2a1f4746e84c4ce8ac8ca522d0
robertsdana <dmr4of8@gmail.com>
2020-07-16 14:43:48 -0600

    Final weather page

--- js/preston-townpage.js
+++ js/preston-townpage.js
@@ -1,26 +1,45 @@
-const getDate = document.createElement('p');
+const getDate = document.createElement("p");
 getDate.innerText = new Date().toDateString();
-const dateElement = document.getElementsByTagName('footer')[0];
-dateElement.appendChild(getDate)
-
+const dateElement = document.getElementsByTagName("footer")[0];
+dateElement.appendChild(getDate);
+const pageTitle = document.getElementsByClassName("page-title")[0].innerText;
 const menuButton = document.querySelector(".item-1");
 const hamburgerMenu = document.querySelector(".hamburger-menu");
 
-menuButton.addEventListener('click', () => {
-    hamburgerMenu.classList.toggle('squashed')
-}, false)
+menuButton.addEventListener(
+  "click",
+  () => {
+    hamburgerMenu.classList.toggle("squashed");
+  },
+  false
+);
 
 const today = new Date();
 const dayNum = today.getDay();
 
 const alertBanner = document.querySelector(".important-alert");
 if (dayNum !== 5) {
-    alertBanner.classList.add("hidden");
+  alertBanner.classList.add("hidden");
 }
 
-const appID = '75ec8f9280bbd969ed410bd87a0de696';
-const prestonID = '5604473';
-const apiURL = `http://api.openweathermap.org/data/2.5/weather?id=${prestonID}&units=imperial&appid=${appID}`;
+const appID = "75ec8f9280bbd969ed410bd87a0de696";
+let townID = ""
+switch(pageTitle) {
+    case "Fish Haven Idaho":
+        townID = "5585000"
+        break;
+    case "Soda Springs Idaho":
+        // Caribou county doesn't have an ID I could find so I used Bear Lake which is adjacent
+        townID = "5585000"
+        break;
+    case "Preston Idaho":
+        townID =  "5604473"
+        break;
+    default:
+        townID = "5604473";
+}
+
+const apiURL = `https://api.openweathermap.org/data/2.5/weather?id=${townID}&units=imperial&appid=${appID}`;
 fetch(apiURL)
   .then((response) => response.json())
   .then((jsObject) => {
@@ -28,9 +47,9 @@ fetch(apiURL)
     document.getElementById("high-temp").innerText = jsObject.main.temp_max;
     document.getElementById("humidity").innerText = jsObject.main.humidity;
     document.getElementById("wind-speed").innerText = jsObject.wind.speed;
-});
+  });
 
-var weekdays = new Array(7)
+var weekdays = new Array(7);
 weekdays[0] = "Sun";
 weekdays[1] = "Mon";
 weekdays[2] = "Tue";
@@ -39,23 +58,106 @@ weekdays[4] = "Thu";
 weekdays[5] = "Fri";
 weekdays[6] = "Sat";
 
-const forecastURL = `http://api.openweathermap.org/data/2.5/forecast?id=${prestonID}&units=imperial&appid=${appID}`;
+const forecastURL = `https://api.openweathermap.org/data/2.5/forecast?id=${townID}&units=imperial&appid=${appID}`;
 fetch(forecastURL)
   .then((response) => response.json())
   .then((jsObject) => {
-    for (i=0; i < jsObject.list.length; i++) {
-        if (jsObject.list[i].dt_txt.includes("18:00:00")) {
-            const forecastDate = new Date(jsObject.list[i].dt_txt);
-            const today = new Date();
-            const forecastNumber = Math.ceil((forecastDate - today) / (1000 * 3600 * 24));
-            document.getElementById(`fore-${forecastNumber}`).innerText = Math.floor(jsObject.list[i].main.temp);
-            document.getElementById(`day-${forecastNumber}`).innerText = weekdays[forecastDate.getDay()];
-            const imagesrc = 'https://openweathermap.org/img/w/' + jsObject.list[i].weather[0].icon + '.png';
-            const desc = jsObject.list[i].weather[0].description;
-            document.getElementById(`icon-${forecastNumber}`).setAttribute('src', imagesrc);  
-            document.getElementById(`icon-${forecastNumber}`).setAttribute('alt', desc);
-        }
+    for (i = 0; i < jsObject.list.length; i++) {
+      if (jsObject.list[i].dt_txt.includes("18:00:00")) {
+        const forecastDate = new Date(jsObject.list[i].dt_txt);
+        const today = new Date();
+        const forecastNumber = Math.ceil(
+          (forecastDate - today) / (1000 * 3600 * 24)
+        );
+        document.getElementById(
+          `fore-${forecastNumber}`
+        ).innerText = Math.floor(jsObject.list[i].main.temp);
+        document.getElementById(`day-${forecastNumber}`).innerText =
+          weekdays[forecastDate.getDay()];
+        const imagesrc =
+          "https://openweathermap.org/img/w/" +
+          jsObject.list[i].weather[0].icon +
+          ".png";
+        const desc = jsObject.list[i].weather[0].description;
+        document
+          .getElementById(`icon-${forecastNumber}`)
+          .setAttribute("src", imagesrc);
+        document
+          .getElementById(`icon-${forecastNumber}`)
+          .setAttribute("alt", desc);
+      }
     }
-});
+  });
 
+const requestURL = "https://byui-cit230.github.io/weather/data/towndata.json";
 
+fetch(requestURL)
+  .then(function (response) {
+    return response.json();
+  })
+  .then(function (jsonObject) {
+    const towns = jsonObject["towns"];
+    for (let i = 0; i < towns.length; i++) {
+      if (pageTitle.includes(towns[i].name)) {
+        let card = document.createElement("section");
+        let h3 = document.createElement("h3");
+        h3.textContent = towns[i].name + " Events";
+        card.appendChild(h3);
+        for (let j = 0; j < towns[i].events.length; j++) {
+            let event = document.createElement("p");
+            event.textContent = towns[i].events[j];
+            card.appendChild(event);
+        }
+        document.querySelector("div.event-card").appendChild(card);
+      }
+      if (pageTitle == "Home") {
+          let image = null;
+        if (towns[i].name == "Fish Haven") {
+            image = document.querySelector(".fish-haven-picture");
+            image.setAttribute('src', towns[i].photo);
+            image.setAttribute('alt', towns[i].name);
+            const name = document.querySelectorAll(".town-name.fh");
+            name[0].textContent = towns[i].name;
+            const slogan = document.querySelectorAll(".town-slogan.fh");
+            slogan[0].textContent = towns[i].motto;
+            const year = document.querySelectorAll(".year.fh");
+            year[0].textContent = towns[i].yearFounded;
+            const pop = document.querySelectorAll(".pop-num.fh");
+            pop[0].textContent = towns[i].currentPopulation;
+            const rain = document.querySelectorAll(".rain-inches.fh");
+            rain[0].textContent = towns[i].averageRainfall;
+        }
+        if (towns[i].name == "Soda Springs") {
+            image = document.querySelector(".soda-springs-picture");
+            image.setAttribute('src', towns[i].photo);
+            image.setAttribute('alt', towns[i].name);
+            const name = document.querySelectorAll(".town-name.ss");
+            console.log(name);
+            name[0].textContent = towns[i].name;
+            const slogan = document.querySelectorAll(".town-slogan.ss");
+            slogan[0].textContent = towns[i].motto;
+            const year = document.querySelectorAll(".year.ss");
+            year[0].textContent = towns[i].yearFounded;
+            const pop = document.querySelectorAll(".pop-num.ss");
+            pop[0].textContent = towns[i].currentPopulation;
+            const rain = document.querySelectorAll(".rain-inches.ss");
+            rain[0].textContent = towns[i].averageRainfall;
+        }
+        if (towns[i].name == "Preston")    {
+            image = document.querySelector(".preston-picture");
+            image.setAttribute('src', towns[i].photo);
+            image.setAttribute('alt', towns[i].name);
+            const name = document.querySelectorAll(".town-name.pres");
+            name[0].textContent = towns[i].name;
+            const slogan = document.querySelectorAll(".town-slogan.pres");
+            slogan[0].textContent = towns[i].motto;
+            const year = document.querySelectorAll(".year.pres");
+            year[0].textContent = towns[i].yearFounded;
+            const pop = document.querySelectorAll(".pop-num.pres");
+            pop[0].textContent = towns[i].currentPopulation;
+            const rain = document.querySelectorAll(".rain-inches.pres");
+            rain[0].textContent = towns[i].averageRainfall;
+        }
+      }
+    }
+  });
